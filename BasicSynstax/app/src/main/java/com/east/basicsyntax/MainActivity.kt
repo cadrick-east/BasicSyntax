59e9e086212899c60575dd6df554b61c3a8a2da8
East <eaststate93@gmail.com>
2021-07-12 22:20:44 +0900

    Initial commit

---
+++ BasicSynstax/app/src/main/java/com/east/basicsyntax/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.east.basicsyntax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("BasicSyntax","print log. method = Log.d")
+    }
+}
